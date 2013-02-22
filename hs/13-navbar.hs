14efbdb3f249a958964da142116d3400c0942653
Vladimir Zakharov <zakharov.vv@gmail.com>
2013-02-22 11:58:46 +0400

    Глава 13 (ПРПРВ): вынес примеры в отдельные файлы

---
+++ hs/13-navbar.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, TemplateHaskell, FlexibleContexts,
+             QuasiQuotes, TypeFamilies, MultiParamTypeClasses, GADTs #-}
+import Yesod
+import Database.Persist.Sqlite
+import Data.Text (Text)
+import Data.Time
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
+Link
+    title Text
+    url Text
+    added UTCTime
+|]
+
+data LinksExample = LinksExample ConnectionPool
+
+mkYesod "LinksExample" [parseRoutes|
+/ RootR GET
+/add-link AddLinkR POST
+|]
+
+instance Yesod LinksExample
+
+instance RenderMessage LinksExample FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+instance YesodPersist LinksExample where
+    type YesodPersistBackend LinksExample = SqlPersist
+    runDB db = do
+        LinksExample pool <- getYesod
+        runSqlPool db pool
+
+getRootR :: Handler RepHtml
+getRootR = defaultLayout [whamlet|
+<form method=post action=@{AddLinkR}>
+    <p>
+        Add a new link to #
+        <input type=url name=url value=http://>
+        \ titled #
+        <input type=text name=title>
+        \ #
+        <input type=submit value="Add link">
+<h2>Existing links
+^{existingLinks}
+|]
+
+existingLinks :: Widget
+existingLinks = do
+    links <- lift $ runDB $ selectList [] [LimitTo 5, Desc LinkAdded]
+    [whamlet|
+<ul>
+    $forall Entity _ link <- links
+        <li>
+            <a href=#{linkUrl link}>#{linkTitle link}
+|]
+
+postAddLinkR :: Handler ()
+postAddLinkR = do
+    url <- runInputPost $ ireq urlField "url"
+    title <- runInputPost $ ireq textField "title"
+    now <- liftIO getCurrentTime
+    runDB $ insert $ Link title url now
+    setMessage "Link added"
+    redirect RootR
+
+main :: IO ()
+main = withSqlitePool "links.db3" 10 $ \pool -> do
+    runSqlPool (runMigration migrateAll) pool
+    warpDebug 3000 $ LinksExample pool
