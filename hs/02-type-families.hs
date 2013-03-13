0a4336a1f9593cd4f9d4e015b00bd78d57660760
Vladimir Zakharov <zakharov.vv@gmail.com>
2013-03-13 17:07:08 +0400

    Глава 2: вынес пример в отдельный файл (#101)

---
+++ hs/02-type-families.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TypeFamilies, OverloadedStrings #-}
+
+import Data.Word (Word8)
+import qualified Data.ByteString as S
+import Data.ByteString.Char8 () -- получить обособленно определённый экземпляр IsString
+
+class SafeHead a where
+    type Content a
+    safeHead :: a -> Maybe (Content a)
+
+instance SafeHead [a] where
+    type Content [a] = a
+    safeHead [] = Nothing
+    safeHead (x:_) = Just x
+
+instance SafeHead S.ByteString where
+    type Content S.ByteString = Word8
+    safeHead bs
+        | S.null bs = Nothing
+        | otherwise = Just $ S.head bs
+
+main :: IO ()
+main = do
+    print $ safeHead ("" :: String)
+    print $ safeHead ("hello" :: String)
+
+    print $ safeHead ("" :: S.ByteString)
+    print $ safeHead ("hello" :: S.ByteString)
