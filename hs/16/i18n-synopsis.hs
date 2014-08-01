f3c1914349853b60aff9100044e041ebce8d246e
Vladimir Zakharov <zakharov.vv@gmail.com>
2014-08-01 11:53:39 +0400

    Глава 16: обновление кода, орфография, ёфикация

--- hs/16/i18n-synopsis.hs
+++ hs/16/i18n-synopsis.hs
@@ -1,56 +1,61 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies,
-    MultiParamTypeClasses #-}
-import Yesod
 
-data I18N = I18N
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
 
-mkMessage "I18N" "hs/16/messages" "en"
+data App = App
+
+mkMessage "App" "hs/16/messages" "en"
 
 plural :: Int -> String -> String -> String -> String
 plural 1 x _ _ = x
-plural n _ y z = if n < 5 then y else z
+plural n _ y z | n <= 4 = y
+               | otherwise = z
 
 showInt :: Int -> String
 showInt = show
 
-instance Yesod I18N
+instance Yesod App
 
-instance RenderMessage I18N FormMessage where
+instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
 
-mkYesod "I18N" [parseRoutes|
-/ RootR GET
-/buy BuyR GET
+mkYesod "App" [parseRoutes|
+/     HomeR GET
+/buy  BuyR  GET
 /lang LangR POST
 |]
 
-getRootR :: Handler RepHtml
-getRootR = defaultLayout [whamlet|
-<h1>_{MsgHello}
-<form action=@{BuyR}>
-    _{MsgEnterItemCount}
-    <input type=text name=count>
-    <input type=submit value=_{MsgPurchase}>
-<form action=@{LangR} method=post>
-    _{MsgSwitchLanguage}
-    <select name=lang>
-        <option value=en>English
-        <option value=ru>Russian
-    <input type=submit value=_{MsgSwitch}>
-|]
+getHomeR :: Handler Html
+getHomeR = defaultLayout
+    [whamlet|
+        <h1>_{MsgHello}
+        <form action=@{BuyR}>
+            _{MsgEnterItemCount}
+            <input type=text name=count>
+            <input type=submit value=_{MsgPurchase}>
+        <form action=@{LangR} method=post>
+            _{MsgSwitchLanguage}
+            <select name=lang>
+                <option value=en>English
+                <option value=he>Hebrew
+            <input type=submit value=_{MsgSwitch}>
+    |]
 
-getBuyR :: Handler RepHtml
+getBuyR :: Handler Html
 getBuyR = do
     count <- runInputGet $ ireq intField "count"
-    defaultLayout [whamlet|
-<p>_{MsgItemCount count}
-|]
+    defaultLayout [whamlet|<p>_{MsgItemCount count}|]
 
 postLangR :: Handler ()
 postLangR = do
     lang <- runInputPost $ ireq textField "lang"
     setLanguage lang
-    redirect RootR
+    redirect HomeR
 
 main :: IO ()
-main = warpDebug 3000 I18N
+main = warp 3000 App
+
